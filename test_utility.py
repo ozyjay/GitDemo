d05a5829911c37aeeff72992545fa991d07e98c9
Jason Holdsworth <ozyjay@gmail.com>
2015-09-28 10:54:49 +1000

    Initial Commit - first working version of fact()

---
+++ test_utility.py
@@ -0,0 +1,4 @@
+import utility
+
+print('test fact()')
+print('fact(3): expected 6, actual:', utility.fact(3))
---
+++ utility.py
@@ -0,0 +1,5 @@
+def fact(n):
+    result = 1
+    for value in range(1, n + 1):
+        result *= value
+    return result
